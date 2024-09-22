201a215095665906bac29b35c8080bee3bf4d113
rezasheikhloo <m.rezasheikhloo@gmail.com>
2024-09-22 20:25:16 +0330

    s

--- model/database.sql
+++ model/database.sql
@@ -1,9 +1,9 @@
 create database mft;
 
-create table mft.user_tbl(
-    username varchar(20) primary key ,
-    password varchar(20),
-    name varchar(20),
-    family varchar(20),
-    is_active tinyint
+create table mft.client_tbl(
+      username varchar(20) primary key ,
+      password varchar(20),
+      name varchar(30),
+      family varchar(20),
+      birth_date Date('yyyy-MM-dd')
 );
