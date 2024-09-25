6303a7b7f828bbc41a9adc4f9a012f302ead51a2
rezasheikhloo <m.rezasheikhloo@gmail.com>
2024-09-25 22:31:16 +0330

    h

---
+++ model/entity/client.sql
@@ -0,0 +1,9 @@
+create table mft.client_tbl
+(
+    id       int primary key auto_increment,
+    name     varchar(30),
+    family   varchar(30),
+    username varchar(30),
+    password varchar(30)
+);
+
