76f6cc6959bd2091e0cffeab1fc46a643f9af0d7
rezasheikhloo <m.rezasheikhloo@gmail.com>
2024-09-29 17:15:37 +0330

    best

--- view/transaction_view.py
+++ view/transaction_view.py
@@ -1,8 +1,11 @@
 from tkinter import *
 from tkinter import ttk
-
+from controller.transaction_controller import TransactionController
+from commponent import *
+def refresh_transaction():
 
 win = Tk()
+win.geometry("700x500")
 
 table = ttk.Treeview(win, columns=(1, 2, 3, 4, 5, 6), show="headings")
 table.column(1, width=50)
@@ -16,8 +19,8 @@ table.heading(1, text="Id")
 table.heading(2, text="Status")
 table.heading(3, text="Amount")
 table.heading(4, text="Datetime")
-table.heading(5, text="Source_account")
-table.heading(6, text="Destination_account")
+table.heading(5, text="Source Account")
+table.heading(6, text="Destination Account")
 
 
 
